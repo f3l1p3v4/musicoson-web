d884b93d5f9c16e461b468acb0f8e70dedf27fb3
f3l1p3v4 <fvaldez018@gmail.com>
2025-07-24 12:48:31 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { router } from './routes'
 export function App() {
   return (
     <div>
-      <ThemeProvider defaultTheme="light" storageKey="pizzashop-theme">
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Toaster richColors />
         <RouterProvider router={router} />
       </ThemeProvider>
