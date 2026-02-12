94bbeb6e5dc183f9e85f4ee953987d8681385135
f3l1p3v4 <fvaldez018@gmail.com>
2026-02-12 14:11:54 -0400

    changed info cars dash

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import path from 'node:path'
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -41,6 +39,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          excel: ['xlsx'],
+          utils: ['date-fns', 'zustand', 'axios', 'clsx'],
+        },
+      },
     },
   },
 })
